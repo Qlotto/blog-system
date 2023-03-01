39e1ad42cc58f1635c93a29b4c5361b3f48d4612
= <=>
2023-03-01 16:34:11 +0300

    mvc_architecture

---
+++ application/models/model_registration.php
@@ -0,0 +1,9 @@
+<?php
+
+class Model_Registration extends Model
+{
+	public function get_data()
+	{	
+	
+	}
+}
--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -5,20 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/login.css">
-    <title>login</title>
+    <link rel="stylesheet" href="./css/registration.css">
+    <title>registration</title>
 </head>
 <body>
     <div class="page-one">
         <nav class="nav">
-            <a href="registration.html">Регистрация</a>
+            <a href="login">Логин</a>
         </nav>
 
         <div class="login">
             <form action="#" >
                 <input type="text" name="name" placeholder="Имя">
                 <input type="password" name="password" placeholder="Пароль">
-                <input type="submit" name="Login" value="Логин">
+                <input type="submit" name="registration" value="Регистрация">
             </form>
         </div>
     </div>
