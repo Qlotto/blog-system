ef2c6ffcb0521d57719ac6ca3521583eb762287d
= <=>
2023-03-01 20:21:10 +0300

    edit

--- application/core/route.php
+++ application/core/route.php
@@ -4,30 +4,24 @@ class Route
 {
 	static function start()
 	{
-		// контроллер и действие по умолчанию
 		$controller_name = 'Login';
 		$action_name = 'index';
 		
 		$routes = explode('/', $_SERVER['REQUEST_URI']);
 
-		// получаем имя контроллера
 		if ( !empty($routes[1]) )
 		{	
 			$controller_name = $routes[1];
 		}
 		
-		// получаем имя экшена
 		if ( !empty($routes[2]) )
 		{
 			$action_name = $routes[2];
 		}
 
-		// добавляем префиксы
 		$model_name = 'Model_'.$controller_name;
 		$controller_name = 'Controller_'.$controller_name;
 		$action_name = 'action_'.$action_name;
-
-		// подцепляем файл с классом модели (файла модели может и не быть)
 
 		$model_file = strtolower($model_name).'.php';
 		$model_path = "application/models/".$model_file;
@@ -36,7 +30,6 @@ class Route
 			include "application/models/".$model_file;
 		}
 
-		// подцепляем файл с классом контроллера
 		$controller_file = strtolower($controller_name).'.php';
 		$controller_path = "application/controllers/".$controller_file;
 		if(file_exists($controller_path))
@@ -45,25 +38,18 @@ class Route
 		}
 		else
 		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
 			Route::ErrorPage404();
 		}
 		
-		// создаем контроллер
 		$controller = new $controller_name;
 		$action = $action_name;
 		
 		if(method_exists($controller, $action))
 		{
-			// вызываем действие контроллера
 			$controller->$action();
 		}
 		else
 		{
-			// здесь также разумнее было бы кинуть исключение
 			Route::ErrorPage404();
 		}
 	
